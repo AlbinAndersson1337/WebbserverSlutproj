224fd7172339ea3f5b316e12224a0677c163361e
AlbinAndersson1337 <118989634+AlbinAndersson1337@users.noreply.github.com>
2024-05-24 20:55:56 +0200

    fdssfd

--- public/todo.js
+++ public/todo.js
@@ -1,3 +1,52 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const myListContainer = document.querySelector(".todo-lists");
+let selectedTodoID = null;
+
+
+const todoLists = document.querySelectorAll('.todo-lists .list-item');
+todoLists.forEach(div => {
+    div.addEventListener('click', function() {
+      //Stylar färgen på alla andra divar till vanligt (detta är bara för att det ska se snyggt ut)
+      mediaDivs.forEach(otherDiv => {
+          otherDiv.style.backgroundColor = ""; 
+      });
+
+      //stylar färgen på den div som klickas på till röd (detta är bara för att det ska se snyggt ut)
+      this.style.backgroundColor = "red"; 
+
+      //Sparar listID för den klickade diven
+      selectedTodoID = this.querySelector('.list-item').textContent;
+    });
+});
+
+const deleteButton = document.querySelector('.btn-delete');
+
+deleteButton.addEventListener('click', function() {
+//Om en todo är vald så skickas den till backend för att tas bort
+if (selectedTodoID) {
+  console.log("clicked")
+  //Inställningar för hur den ska skickas
+  const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({listID: selectedTodoID})
+  };
+  fetch('/delete', options)
+      .then(response => {
+          if (response.ok) {
+              location.reload();
+          } else {
+              throw new Error('Failed to send data');
+          }
+      })
+    }
+ });
+});
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addListButton = document.querySelector(".btn-add");
   const listNameInput = document.querySelector("#list-name-input");
